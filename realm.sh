c48366a1363f422454939540a821b3e53e5dec97
gdyan <120347640+gdyan2022@users.noreply.github.com>
2024-05-27 11:21:43 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -7,6 +7,8 @@ chmod +x /usr/local/bin/realm
 mkdir -p /etc/realm/
 cat > /etc/realm/config.toml <<EOF
 [[endpoints]]
+# listen = "0.0.0.0:58081"
+# remote = "8.8.8.8:443"
 
 EOF
 cat > /etc/systemd/system/realm.service <<EOF
