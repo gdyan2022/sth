3f047b9fd53874b574b09ad19a35b2b9942ebd84
gdyan <120347640+gdyan2022@users.noreply.github.com>
2024-10-25 10:45:44 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O /tmp/realm.tar.gz https://github.com/zhboner/realm/releases/latest/realm-x86_64-unknown-linux-gnu.tar.gz
+wget -O /tmp/realm.tar.gz https://github.com/zhboner/realm/releases/latest/download/realm-x86_64-unknown-linux-gnu.tar.gz
 tar -xf /tmp/realm.tar.gz -C /usr/local/bin/
 rm /tmp/realm.tar.gz
 chmod +x /usr/local/bin/realm
