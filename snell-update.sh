60ffc3fc3c4dc924d8acaa4ba5d42a640ac2647b
gdyan <120347640+gdyan2022@users.noreply.github.com>
2025-07-01 16:40:10 +0800

    Create snell-update.sh

---
+++ snell-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -O /tmp/snell-server.zip https://dl.nssurge.com/snell/snell-server-v5.0.0b1-linux-amd64.zip
+cd /tmp/
+unzip snell-server.zip
+chmod +x snell-server
+rm /tmp/snell-server.zip
+systemctl stop snell-server
+cp /tmp/snell-server /usr/local/bin/
+
+systemctl start snell-server
+systemctl status snell-server
